8523dd2b7c9d4e1dfd3b7e96bcb0e5eb61672cf1
Arne Caspari <arne.caspari@theimagingsource.com>
2024-08-30 16:07:03 +0200

    Add service file and launch script

---
+++ launch-gst-mipi-demo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if test -z "$XDG_RUNTIME_DIR"; then
+    export XDG_RUNTIME_DIR=/var/run/user/`id -u`
+    if ! test -d "$XDG_RUNTIME_DIR"; then
+        mkdir --parents $XDG_RUNTIME_DIR
+        chmod 0700 $XDG_RUNTIME_DIR
+    fi
+fi
+# wait for weston
+while [ ! -e  $XDG_RUNTIME_DIR/wayland-1 ] ; do sleep 0.1; done
+sleep 1
+unset DISPLAY
+export WAYLAND_DISPLAY=/var/run/wayland-0
+cd /opt/app ; XDG_RUNTIME_DIR=/var/run/user/0 ./gst-mipi-demo -f /opt/gst-mipi-demo/config.json
